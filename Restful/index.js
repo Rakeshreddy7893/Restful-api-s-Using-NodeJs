6f91c5a1758c417afc0c6b5a8542ee761a8a3c12
Rakeshreddy7893 <rakeshreddykalagiri@gmail.com>
2024-04-23 13:31:35 +0530

    Done with PUT and DELETE request

--- Restful/index.js
+++ Restful/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const path = require('path');
+const{v4:uuidv4} = require('uuid');
+const methodOverride = require('method-override');
+
 
 //for all  requests it will respond
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"));
@@ -17,14 +21,17 @@ let posts = [
 
 
     {
+        id:uuidv4(),
         username: "apnacollage",
         content:"I love coding!"
     },
     {
+        id:uuidv4(),
         username: "shradhakhapra",
         content:"Hardwork is importrant"
     },
     {
+        id:uuidv4(),
         username: "rahulkumar",
         content:"i got selected for my 1st intership"
     },
@@ -42,10 +49,39 @@ res.render("new.ejs");
 
 app.post('/posts',(req,res)=>{
     let{username,content} = req.body;
-    posts.push({username,content});
-    console.log(req.body);
+    let id = uuidv4();
+    posts.push({id,username,content});
     res.redirect("/posts")
 })
+
+app.get('/posts/:id',(req,res)=>{
+   let {id} = req.params;
+   console.log(id);
+   let post = posts.find((p)=> id ===p.id);
+   res.render("show.ejs",{post});
+})
+
+app.patch('/posts/:id',(req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id ===p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts"); 
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id === p.id);
+    res.render("edit.ejs",{post});
+});
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+     posts = posts.filter((p)=> id !== p.id);
+     res.redirect("/posts");
+
+});
 
 app.listen(port,()=>{
     console.log(`listening at ${port}`);
